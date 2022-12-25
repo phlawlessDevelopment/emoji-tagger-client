e3b42b112133883256e7dda65527600c7d0df4b0
phlawlessDevelopment <phalyce@outlook.com>
2022-12-25 17:08:20 +0000

    replaced localhost

--- src/components/Tagger.vue
+++ src/components/Tagger.vue
@@ -78,7 +78,7 @@ export default {
     const types = document.getElementById("types");
     
     fetch(
-      `http://emojitagger.pythonanywhere.com//api/emoji/?count=${this.count}&types=${types.value}`
+      `https://emojitagger.pythonanywhere.com/api/emoji/?count=${this.count}&types=${types.value}`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -94,7 +94,7 @@ export default {
         this.count = event.target.value;
       }
       fetch(
-        `http://emojitagger.pythonanywhere.com//api/emoji/?count=${this.count}&types=${types.value}`
+        `https://emojitagger.pythonanywhere.com/api/emoji/?count=${this.count}&types=${types.value}`
       )
         .then((response) => response.json())
         .then((data) => {
@@ -142,7 +142,7 @@ export default {
           tags: tags,
         };
 
-        fetch(`http://emojitagger.pythonanywhere.com//api/emoji/`, {
+        fetch(`https://emojitagger.pythonanywhere.com/api/emoji/`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
